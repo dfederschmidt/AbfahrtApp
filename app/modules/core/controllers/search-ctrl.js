524ad651b142f6dba0b912cabe42a0949503b3d1
Daniel <daniel.federschmidt@googlemail.com>
2015-01-18 15:27:21 +0100

    Added departure Icon + internet error handlin

--- app/modules/core/controllers/search-ctrl.js
+++ app/modules/core/controllers/search-ctrl.js
@@ -9,16 +9,10 @@
 angular
     .module('core')
     .controller('SearchController', [
-        '$scope','$http',
-        function($scope,$http) {
+        '$scope',
+        function($scope) {
           var init = function() {
-            $http.get('http://syntiux.de/abfahrt/stations').
-              success(function (data, status, headers, config) {
-                $scope.stations = data;
-              }).
-              error(function (data, status, headers, config) {
-                // log error
-              });
+            $scope.stations = stationRegister;
           }
           init();
 
--- app/modules/core/controllers/station.js
+++ app/modules/core/controllers/station.js
@@ -12,26 +12,31 @@ angular
         '$scope','$stateParams','$http',
         function($scope,$stateParams,$http) {
           var init = function() {
-            $http.get('http://syntiux.de/abfahrt/stations/' + $stateParams.id + "/departures").
-              success(function (data, status, headers, config) {
-                if(data.length == 0){
-                  $scope.noService = "In nächster Zeit halten an dieser Station keine Busse.";
-                }
-                else {
-                  $scope.departures = data;
-                }
+              $http.get('http://syntiux.de/abfahrt/stations/' + $stateParams.id + "/departures").
+                success(function (data, status, headers, config) {
+                  if (data.length == 0) {
+                    var networkState = navigator.connection.type;
+                    if(networkState == Connection.NONE){
+                      $scope.noService = "Bitte stellen Sie eine Internetverbindung her";
+                    }else{
+                      $scope.noService = "In nächster Zeit halten an dieser Station keine Busse.";
+                    }
+                  }
+                  else {
+                    $scope.departures = data;
+                  }
                   $http.get('http://syntiux.de/abfahrt/stations/' + $stateParams.id).
-                  success(function (data, status, headers, config) {
-                    $scope.stationInformation = data;
-                  }).
-                  error(function (data, status, headers, config) {
-                    // log error
-                  });
-              }).
-              error(function (data, status, headers, config) {
-                // log error
-              });
-          }
+                    success(function (data, status, headers, config) {
+                      $scope.stationInformation = data;
+                    }).
+                    error(function (data, status, headers, config) {
+                      $scope.noService = "Haltestelleninformationen konnten nicht geladen werden.";
+                    });
+                }).
+                error(function (data, status, headers, config) {
+                  $scope.noService = "Abfahrtszeiten konnten nicht geladen werden.";
+                });
+            }
           init();
         }
 ]);
